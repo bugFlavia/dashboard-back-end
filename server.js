e4b1e2133f2743c7d32867796650736299d6c392
Flavia Nunes <flavia.morais@estudante.ifms.edu.br>
2025-04-04 16:53:35 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,216 +1,216 @@
 const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const jwt = require('jsonwebtoken');
-const cookieParser = require('cookie-parser');
-const connectToOdbc = require('./config/odbcConnection');
+	const cors = require('cors');
+	const bodyParser = require('body-parser');
+	const jwt = require('jsonwebtoken');
+	const cookieParser = require('cookie-parser');
+	const connectToOdbc = require('./config/odbcConnection');
 
-const User = require('./models/user');
+	const User = require('./models/user');
 
-const app = express();
-const port = process.env.PORT;
-const SECRET_KEY = process.env.SECRET_KEY;
-const allowedOrigins = process.env.CLIENT_URL
-
-
-app.use(cors({
-  origin: "*",
-  credentials: true
-}));
-
-app.use(bodyParser.json());
-app.use(cookieParser());
-app.use(express.json());
-
-async function validarCNPJ(req, res, next) {
-  const { is_admin, cnpj } = req.body;
-
-  if (cnpj === null && is_admin == 0) {
-    return res.status(400).json({ error: 'Apenas administradores podem ter CNPJ como "null".' });
-  }
-
-  if (cnpj) {
-    const usuarioExistente = await User.findOne({ where: { cnpj } });
-    if (usuarioExistente) {
-      return res.status(400).json({ error: 'O CNPJ precisa ser único.' });
-    }
-  }
-
-  next();
-}
-
-function getUltimoDiaMes(ano, mes) {
-  return new Date(ano, mes, 0).getDate();
-}
-
-const codiNatExcluidos = new Set([
-  1116, 1151, 1152, 1153, 1154, 1201, 1202, 1203, 1204, 1205, 1206, 1207, 1208, 1209, 1408, 1409, 1410, 1411, 1414, 1415, 1451, 1452, 1501, 1503, 1504, 1552, 1553, 1554, 1555, 1556, 1557, 1601, 1602, 1603, 1604, 1605, 1658, 1659, 1660, 1661, 1662, 1663, 1664, 1901, 1902, 1903, 1904, 1905, 1906, 1907, 1908, 1909, 1910, 1913, 1914, 1916, 1917, 1918, 1919, 1921, 1922, 1923, 1924, 1925, 1926, 1931, 2151, 2152, 2153, 2154, 2201, 2202, 2203, 2204, 2205, 2206, 2207, 2208, 2209, 2408, 2409, 2410, 2411, 2414, 2415, 2501, 2503, 2504, 2551, 2552, 2553, 2554, 2555, 2556, 2557, 2603, 2658, 2659, 2660, 2661, 2662, 2663, 2664, 2901, 2902, 2903, 2904, 2905, 2906, 2907, 2908, 2909, 2910, 2913, 2914, 2915, 2916, 2917, 2918, 2919, 2921, 2922, 2923, 2924, 2925, 2931, 3201, 3202, 3205, 3206, 3207, 3211, 3503, 3553, 5124, 5125, 5151, 5152, 5153, 5155, 5156, 5201, 5202, 5205, 5206, 5207, 5208, 5209, 5210, 5408, 5409, 5410, 5411, 5412, 5413, 5414, 5415, 5451, 5501, 5502, 5503, 5552, 5553, 5554, 5555, 5556, 5557, 5601, 5602, 5603, 5605, 5658, 5659, 5660, 5661, 5662, 5663, 5664, 5665, 5666, 5901, 5902, 5903, 5904, 5905, 5906, 5907, 5909, 5913, 5916, 5917, 5918, 5919, 5921, 5922, 5923, 5924, 5925, 5926, 5929, 5931, 6124, 6125, 6151, 6152, 6153, 6156, 6201, 6202, 6205, 6206, 6207, 6208, 6209, 6210, 6408, 6409, 6410, 6411, 6412, 6413, 6414, 6415, 6501, 6502, 6503, 6551, 6552, 6553, 6554, 6555, 6556, 6557, 6603, 6658, 6659, 6660, 6661, 6662, 6663, 6664, 6665, 6666, 6901, 6902, 6903, 6904, 6905, 6906, 6907, 6909, 6913, 6916, 6917, 6918, 6919, 6921, 6922, 6923, 6924, 6925, 6929, 6931
-]);
-
-function filtrarResultados(resultados) {
-  const excluidos = resultados.filter(row => codiNatExcluidos.has(parseInt(row.codi_nat)));
-  const somaExcluida = excluidos.reduce((acc, row) => acc + (row.vcon_ent || row.vcon_sai), 0);
-  return { filtrados: resultados.filter(row => !codiNatExcluidos.has(parseInt(row.codi_nat))), somaExcluida, excluidos };
-}
-
-async function filtrarRubricas() {
-  try {
-    const odbcConnection = await connectToOdbc(); 
-    const query = `
-      SELECT i_eventos
-      FROM bethadba.foeventos
-      WHERE LOWER(nome) LIKE '%horas extras%'
-    `;
-
-    const resultados = await odbcConnection.query(query);
-    const arrayFiltrado = resultados.map(row => row.i_eventos);
-    return arrayFiltrado;
-  } catch (error) {
-    console.error("Erro ao filtrar rubricas:", error);
-    throw error;
-  }
-}
-
-async function filtrarUsuario(req) {
-  try {
-    const isAdmin = req.user.is_admin; 
-    let codigosEmpresa = [];
-
-    if (isAdmin === 0) {
-      codigosEmpresa = req.user.codi_emp || []; 
-    } else {
-      codigosEmpresa = req.body.codigo_empresa || []; 
-    }
-
-    return codigosEmpresa;
-  } catch (error) {
-    console.error("Erro ao filtrar usuário:", error);
-    throw new Error("Erro ao processar o filtro de usuário");
-  }
-}
-
-module.exports = filtrarUsuario;
-
-function authMiddleware(req, res, next) {
-  console.log("Cabeçalhos recebidos:", req.headers);
-
-  const authHeader = req.headers['authorization'];
-  if (!authHeader) {
-    return res.status(401).json({ error: 'Acesso negado. Faça login.' });
-  }
-
-  const token = authHeader.split(" ")[1]; // Garante que pega só o token
-  console.log("Token recebido:", token);
-
-  try {
-    jwt.verify(token, SECRET_KEY, (err, user) => {
-      if (err) {
-        console.error("Erro ao verificar token:", err);
-        return res.status(403).json({ error: 'Token inválido ou expirado.' });
-      }
-      req.user = user;
-      next();
-    });
-  } catch (err) {
-    console.error("Erro inesperado ao verificar token:", err);
-    return res.status(403).json({ error: 'Token inválido.' });
-  }
-}
+	const app = express();
+	const port = process.env.PORT;
+	const SECRET_KEY = process.env.SECRET_KEY;
+	const allowedOrigins = process.env.CLIENT_URL
 
 
-app.post('/login', async (req, res) => {
-    const { email, senha } = req.body;
-    try {    
-      const user = await User.findOne({ where: { email, senha } });
-      if (user) {
-        const token = jwt.sign({id: user.id, email: user.email, codi_emp: user.codi_emp, is_admin: user.is_admin}, SECRET_KEY, { expiresIn: '1h' });
-        res.json({token});
-      }else{
-        res.status(401).json({ error: 'Email ou senha inválidos' });
-      }
-    } catch (error) {
-      res.status(500).json({ error: 'Erro ao fazer login', details: error.message });
-    }
-});
+	app.use(cors({
+	  origin: "*",
+	  credentials: true
+	}));
 
-app.get('/users', authMiddleware, async (req, res) => {
-  try {
-    const users = await User.findAll();
-    res.json(users);
-  } catch (error) {
-    res.status(500).json({ error: 'Erro ao listar usuários', details: error.message });
-  }
-});
+	app.use(bodyParser.json());
+	app.use(cookieParser());
+	app.use(express.json());
 
-app.get('/empresas', authMiddleware, async (req, res) => {
-  try {
-    const users = await User.findAll({ where: { is_admin: 0 } });
-    res.json(users);
-  } catch (error) {
-    res.status(500).json({ error: 'Erro ao listar empresas', details: error.message });
-  }
-});
+	async function validarCNPJ(req, res, next) {
+	  const { is_admin, cnpj } = req.body;
 
-app.post('/user', authMiddleware, validarCNPJ, async (req, res) => {
-  try {
-    const { nome, nome_empresa, cpf, cnpj, codi_emp, celular, email, senha, is_admin} = req.body;
+	  if (cnpj === null && is_admin == 0) {
+		return res.status(400).json({ error: 'Apenas administradores podem ter CNPJ como "null".' });
+	  }
 
-    const user = await User.create({ 
-      nome, 
-      nome_empresa, 
-      cpf, 
-      cnpj, 
-      codi_emp, 
-      celular, 
-      email, 
-      senha, 
-      is_admin: 0
-    });
+	  if (cnpj) {
+		const usuarioExistente = await User.findOne({ where: { cnpj } });
+		if (usuarioExistente) {
+		  return res.status(400).json({ error: 'O CNPJ precisa ser único.' });
+		}
+	  }
 
-    res.status(201).json(user);
-    console.log("cadastro realizado com sucesso")
-  } catch (error) {
-    console.error("Erro ao criar usuário:", error);
-    res.status(500).json({ error: 'Erro ao criar usuário', details: error.message });
-  }
-});
+	  next();
+	}
 
-app.get('/listaEmpresas', authMiddleware, async (req, res) => {
-  try {
-    const odbcConnection = await connectToOdbc();
-    const empresas = await odbcConnection.query(`
-      SELECT razao_emp, rleg_emp, cpf_leg_emp, cgce_emp, codi_emp, dddf_emp, fone_emp, email_emp
-      FROM bethadba.geempre
-      WHERE codi_emp < 500
-    `);
+	function getUltimoDiaMes(ano, mes) {
+	  return new Date(ano, mes, 0).getDate();
+	}
 
-    // Formatando celular e garantindo que codi_emp seja um array válido
-    const empresasFormatadas = empresas.map(empresa => {
-      let codiEmpArray = [];
-  
-      if (empresa.codi_emp) {
-          if (Array.isArray(empresa.codi_emp) && empresa.codi_emp.length > 0) {
-              codiEmpArray = empresa.codi_emp;
-          } else if (typeof empresa.codi_emp === "string" || typeof empresa.codi_emp === "number") {
-              codiEmpArray = [empresa.codi_emp]; // Converte para array se for um único valor
-          }
-      }
-  
-      return {
-          ...empresa,
-          celular: `${empresa.dddf_emp}${empresa.fone_emp}`,
-          codi_emp: codiEmpArray, // Garantir que codi_emp seja sempre um array
-      };
-  });
+	const codiNatExcluidos = new Set([
+	  1116, 1151, 1152, 1153, 1154, 1201, 1202, 1203, 1204, 1205, 1206, 1207, 1208, 1209, 1408, 1409, 1410, 1411, 1414, 1415, 1451, 1452, 1501, 1503, 1504, 1552, 1553, 1554, 1555, 1556, 1557, 1601, 1602, 1603, 1604, 1605, 1658, 1659, 1660, 1661, 1662, 1663, 1664, 1901, 1902, 1903, 1904, 1905, 1906, 1907, 1908, 1909, 1910, 1913, 1914, 1916, 1917, 1918, 1919, 1921, 1922, 1923, 1924, 1925, 1926, 1931, 2151, 2152, 2153, 2154, 2201, 2202, 2203, 2204, 2205, 2206, 2207, 2208, 2209, 2408, 2409, 2410, 2411, 2414, 2415, 2501, 2503, 2504, 2551, 2552, 2553, 2554, 2555, 2556, 2557, 2603, 2658, 2659, 2660, 2661, 2662, 2663, 2664, 2901, 2902, 2903, 2904, 2905, 2906, 2907, 2908, 2909, 2910, 2913, 2914, 2915, 2916, 2917, 2918, 2919, 2921, 2922, 2923, 2924, 2925, 2931, 3201, 3202, 3205, 3206, 3207, 3211, 3503, 3553, 5124, 5125, 5151, 5152, 5153, 5155, 5156, 5201, 5202, 5205, 5206, 5207, 5208, 5209, 5210, 5408, 5409, 5410, 5411, 5412, 5413, 5414, 5415, 5451, 5501, 5502, 5503, 5552, 5553, 5554, 5555, 5556, 5557, 5601, 5602, 5603, 5605, 5658, 5659, 5660, 5661, 5662, 5663, 5664, 5665, 5666, 5901, 5902, 5903, 5904, 5905, 5906, 5907, 5909, 5913, 5916, 5917, 5918, 5919, 5921, 5922, 5923, 5924, 5925, 5926, 5929, 5931, 6124, 6125, 6151, 6152, 6153, 6156, 6201, 6202, 6205, 6206, 6207, 6208, 6209, 6210, 6408, 6409, 6410, 6411, 6412, 6413, 6414, 6415, 6501, 6502, 6503, 6551, 6552, 6553, 6554, 6555, 6556, 6557, 6603, 6658, 6659, 6660, 6661, 6662, 6663, 6664, 6665, 6666, 6901, 6902, 6903, 6904, 6905, 6906, 6907, 6909, 6913, 6916, 6917, 6918, 6919, 6921, 6922, 6923, 6924, 6925, 6929, 6931
+	]);
 
-    console.log("Empresas formatadas:", empresasFormatadas);
+	function filtrarResultados(resultados) {
+	  const excluidos = resultados.filter(row => codiNatExcluidos.has(parseInt(row.codi_nat)));
+	  const somaExcluida = excluidos.reduce((acc, row) => acc + (row.vcon_ent || row.vcon_sai), 0);
+	  return { filtrados: resultados.filter(row => !codiNatExcluidos.has(parseInt(row.codi_nat))), somaExcluida, excluidos };
+	}
 
-    res.json(empresasFormatadas);
-  } catch (error) {
-    console.error("Erro ao buscar empresas:", error);
-    res.status(500).json({ error: 'Erro ao buscar empresas', details: error.message });
-  }
-});
+	async function filtrarRubricas() {
+	  try {
+		const odbcConnection = await connectToOdbc(); 
+		const query = `
+		  SELECT i_eventos
+		  FROM bethadba.foeventos
+		  WHERE LOWER(nome) LIKE '%horas extras%'
+		`;
+
+		const resultados = await odbcConnection.query(query);
+		const arrayFiltrado = resultados.map(row => row.i_eventos);
+		return arrayFiltrado;
+	  } catch (error) {
+		console.error("Erro ao filtrar rubricas:", error);
+		throw error;
+	  }
+	}
+
+	async function filtrarUsuario(req) {
+	  try {
+		const isAdmin = req.user.is_admin; 
+		let codigosEmpresa = [];
+
+		if (isAdmin === 0) {
+		  codigosEmpresa = req.user.codi_emp || []; 
+		} else {
+		  codigosEmpresa = req.body.codigo_empresa || []; 
+		}
+
+		return codigosEmpresa;
+	  } catch (error) {
+		console.error("Erro ao filtrar usuário:", error);
+		throw new Error("Erro ao processar o filtro de usuário");
+	  }
+	}
+
+	module.exports = filtrarUsuario;
+
+	function authMiddleware(req, res, next) {
+	  console.log("Cabeçalhos recebidos:", req.headers);
+
+	  const authHeader = req.headers['authorization'];
+	  if (!authHeader) {
+		return res.status(401).json({ error: 'Acesso negado. Faça login.' });
+	  }
+
+	  const token = authHeader.split(" ")[1]; // Garante que pega só o token
+	  console.log("Token recebido:", token);
+
+	  try {
+		jwt.verify(token, SECRET_KEY, (err, user) => {
+		  if (err) {
+			console.error("Erro ao verificar token:", err);
+			return res.status(403).json({ error: 'Token inválido ou expirado.' });
+		  }
+		  req.user = user;
+		  next();
+		});
+	  } catch (err) {
+		console.error("Erro inesperado ao verificar token:", err);
+		return res.status(403).json({ error: 'Token inválido.' });
+	  }
+	}
+
+
+	app.post('/login', async (req, res) => {
+		const { email, senha } = req.body;
+		try {    
+		  const user = await User.findOne({ where: { email, senha } });
+		  if (user) {
+			const token = jwt.sign({id: user.id, email: user.email, codi_emp: user.codi_emp, is_admin: user.is_admin}, SECRET_KEY, { expiresIn: '1h' });
+			res.json({token});
+		  }else{
+			res.status(401).json({ error: 'Email ou senha inválidos' });
+		  }
+		} catch (error) {
+		  res.status(500).json({ error: 'Erro ao fazer login', details: error.message });
+		}
+	});
+
+	app.get('/users', async (req, res) => {
+	  try {
+		const users = await User.findAll();
+		res.json(users);
+	  } catch (error) {
+		res.status(500).json({ error: 'Erro ao listar usuários', details: error.message });
+	  }
+	});
+
+	app.get('/empresas', async (req, res) => {
+	  try {
+		const users = await User.findAll({ where: { is_admin: 0 } });
+		res.json(users);
+	  } catch (error) {
+		res.status(500).json({ error: 'Erro ao listar empresas', details: error.message });
+	  }
+	});
+
+	app.post('/user', validarCNPJ, async (req, res) => {
+	  try {
+		const { nome, nome_empresa, cpf, cnpj, codi_emp, celular, email, senha, is_admin } = req.body;
+
+		const user = await User.create({ 
+		  nome, 
+		  nome_empresa, 
+		  cpf, 
+		  cnpj, 
+		  codi_emp, 
+		  celular, 
+		  email, 
+		  senha, 
+		  is_admin // Agora usa o valor enviado pelo front-end
+		});
+
+		res.status(201).json(user);
+		console.log("Cadastro realizado com sucesso");
+	  } catch (error) {
+		console.error("Erro ao criar usuário:", error);
+		res.status(500).json({ error: 'Erro ao criar usuário', details: error.message });
+	  }
+	});
+
+	app.get('/listaEmpresas', authMiddleware, async (req, res) => {
+	  try {
+		const odbcConnection = await connectToOdbc();
+		const empresas = await odbcConnection.query(`
+		  SELECT razao_emp, rleg_emp, cpf_leg_emp, cgce_emp, codi_emp, dddf_emp, fone_emp, email_emp
+		  FROM bethadba.geempre
+		  WHERE codi_emp < 500
+		`);
+
+		// Formatando celular e garantindo que codi_emp seja um array válido
+		const empresasFormatadas = empresas.map(empresa => {
+		  let codiEmpArray = [];
+	  
+		  if (empresa.codi_emp) {
+			  if (Array.isArray(empresa.codi_emp) && empresa.codi_emp.length > 0) {
+				  codiEmpArray = empresa.codi_emp;
+			  } else if (typeof empresa.codi_emp === "string" || typeof empresa.codi_emp === "number") {
+				  codiEmpArray = [empresa.codi_emp]; // Converte para array se for um único valor
+			  }
+		  }
+	  
+		  return {
+			  ...empresa,
+			  celular: `${empresa.dddf_emp}${empresa.fone_emp}`,
+			  codi_emp: codiEmpArray, // Garantir que codi_emp seja sempre um array
+		  };
+	  });
+
+		console.log("Empresas formatadas:", empresasFormatadas);
+
+		res.json(empresasFormatadas);
+	  } catch (error) {
+		console.error("Erro ao buscar empresas:", error);
+		res.status(500).json({ error: 'Erro ao buscar empresas', details: error.message });
+	  }
+	});
 
 app.put('/user/:id', authMiddleware, validarCNPJ, async (req, res) => {
   try {
